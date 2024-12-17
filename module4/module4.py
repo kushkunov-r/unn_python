e09d0d816eca8821b32f4ffa6e5300b33c61df4b
kushkunov-r <kushkunov.r@gmail.com>
2024-12-17 13:04:57 +0300

    add module4

---
+++ module4/module4.py
@@ -0,0 +1,27 @@
+command_output = (open("sh_cdp_n_r1.txt", "r")).read()
+
+def parse_cdp_neighbors(a):
+    dict_connect = {}
+    my_hostname = ""
+    
+    for i in a:
+        if i == "#":
+            break
+        else:
+            my_hostname = my_hostname + str(i)
+    
+    a = a.split()
+    
+    for i in range(len(a)):
+        if a[i] == "Port" and a[i+1] == "ID":
+            start = i + 2
+        if a[i] == "Total":
+            end = i
+            
+    while start < end:
+        dict_connect.setdefault((my_hostname, (a[start+1] + a[start+2])), ((a[start], a[start+8] + a[start+9])))
+        start = start + 10
+    
+    return(dict_connect)
+        
+print(parse_cdp_neighbors(command_output))
