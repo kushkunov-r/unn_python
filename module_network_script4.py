f01e14e87076234025eff713555f296dfaaf55eb
kushkunov-r <151659700+kushkunov-r@users.noreply.github.com>
2024-12-20 11:45:02 +0300

    Update module_network_script4.py

---
+++ module_network_script4.py
@@ -0,0 +1,14 @@
+Dict_interface = {
+				"GigabitEthernet0/0": {"IP-address":192.168.1.1, "OK":"YES", "Method":"manual", "Status":"Up", "Protocol":"Up"},
+				"GigabitEthernet0/1": {"IP-address":192.168.2.1, "OK":"YES", "Method":"manual", "Status":"Up", "Protocol":"Up"},
+				"GigabitEthernet0/2": {"IP-address":"unassigned", "OK":"YES", "Method":"manual", "Status":"Down", "Protocol":"Down"},
+				"Serial0/3/0": {"IP-address":192.168.23.1, "OK":"YES", "Method":"manual", "Status":"Up", "Protocol":"Up"},
+				"Serial0/3/1": {"IP-address": "11.12.12.2, "OK":"YES", "Method":"manual", "Status":"Down", "Protocol":"Down"}
+				}
+				
+dict_cdp_neighbors = {
+					"Switch1":{"IP-address":192.168.1.2},
+					"Switch2":{"IP-address":192.168.2.3},
+					"Router_4":{"IP-address":192.168.23.2},
+					"Router_3":{"IP-address": 11.12.12.3}
+					}
