6a95d73ff179c1f786da8e9804f87837dec743d0
kushkunov-r <151659700+kushkunov-r@users.noreply.github.com>
2024-12-20 10:05:18 +0300

    Update module_network_script3.py

---
+++ module_network_script3.py
@@ -0,0 +1,8 @@
+table_routing: {
+				1:{"C": "192.168.1.0", "mask": 255.255.255.0, "interface":"GigabitEthernet0/1"}, 
+				2:{"C": "192.168.2.0", "mask": 255.255.255.0, "interface":"GigabitEthernet0/2"}, 
+				3:{"C": "192.168.3.0", "mask": 255.255.255.0, "interface":"GigabitEthernet0/3"}, 
+				4:{"O": "10.10.1.0", "mask": 255.255.255.0, "interface":"GigabitEthernet0/1"}, 
+				5:{"R": "11.0.0.0", "mask": 255.0.0.0, "interface":"GigabitEthernet0/2"}, 
+				6:{"D": "173.221.14.0", "mask": 255.255.255.240, "interface":"GigabitEthernet0/3"},
+				}
