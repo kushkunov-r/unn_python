798bcaac40584d9e6748a9c1b38e91af80365ee8
kushkunov-r <kushkunov.r@gmail.com>
2025-01-19 11:08:29 +0300

    update

---
+++ 19-01-2025.py
@@ -0,0 +1,16 @@
+import csv
+
+def write_dhcp_snooping_to_csv(filenames, output):
+    output_list = [["switch", "mac", "ip", "vlan", "interface"], ]
+    for i in filenames:
+        hostname = i.split("_")[0]
+        with open(i, "r") as dhcp_snooping:
+            for line in dhcp_snooping:
+                if ":" in line.split()[0]:
+                    output_list.append([hostname, line.split()[0], line.split()[1], line.split()[4], line.split()[5]])
+                    
+    print(output_list)
+
+files = ["sw1_dhcp_snooping.txt", "sw2_dhcp_snooping.txt", "sw3_dhcp_snooping.txt"]
+out = "output_snooping.csv"
+write_dhcp_snooping_to_csv(files, out)
